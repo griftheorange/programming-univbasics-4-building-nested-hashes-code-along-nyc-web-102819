30a6f389ed4e105baf7892951e47b9bb1f14f0a2
Griffin Poole <griffinpoole5@gmail.com>
2019-10-07 14:46:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,8 +2,8 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {name: 'Lord Montague', age: "53"},
-       :matriarch => {},
-       :hero => {},
+       :matriarch => {name: 'Lady Montague', age: '54'},
+       :hero => {name: 'Romeo', age: '15'},
        :hero_friends => {}
      },
      :capulet => {
